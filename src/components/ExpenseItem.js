48ef4bc05e2c3f9214301c3ec4bdd4d83e5b6a53
Daanish <daanish629@gmail.com>
2022-06-22 16:24:25 +0530

    1st section completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  console.log(props);
+  // console.log(props);
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
