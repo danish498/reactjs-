16b188de53665e5aeb7429de3ac43c5ca837508f
Daanish <daanish629@gmail.com>
2022-06-23 18:50:53 +0530

    filter by year use of p to c

--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -6,10 +6,7 @@ const NewExpense = (props) => {
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = { ...enteredExpenseData, id: Math.random().toString() }; // need to understand
 
-    console.log("----Here we took  the data form the 1st data crated");
-    console.log(expenseData);
     props.onAddExpense(expenseData);
-    // "by this method we will be transfarin the data to the onAddExpense props in order to the get the data in PARENTS class"
 
     console.log(
       "by this method we will be transfarin the data to the onAddExpense props in order to the get the data in PARENTS class"
